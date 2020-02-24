8e4d4600b78661429990d9a721f3a773b30a3f24
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-02-24 14:08:14 +0100

    delete

--- nd2shrink/main.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from libtiff import TIFFimage
-
-if __name__ == "__main__":
-        
-    im = TIFFimage(numpy.zeros((10,10), dtype='uint16'))
-    im.write_file('zeros.tif', compression='lzw')
-
-    exit(0)
