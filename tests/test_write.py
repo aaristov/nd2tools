d89b10bf5a204f959e140ac612807751a0695b73
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-02-24 14:09:43 +0100

    cleanup

--- tests/test_write.py
+++ tests/test_write.py
@@ -7,15 +7,12 @@ import os, sys
 def test_save_zeros():
     arr = numpy.random.randint(0, 2**12, (20,256,256), dtype='uint16')
 
-    OK = save.tiff('zeros.tif', arr)
+    _ = save.tiff('zeros.tif', arr)
 
     assert os.path.exists('zeros.tif')
 
-# def read_zeros():
     tif = read.tiff('zeros.tif')
-    # array = numpy.array([t for t in tif.iter_images()])
     numpy.testing.assert_array_equal(arr, tif)
-    # tif.close()
 
 
 
