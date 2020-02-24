7c4d62a144983f664181498e06b2a856e0799077
Andrey  ARISTOV <andrey.aristov@pasteur.frsource>
2020-02-25 00:26:32 +0100

    cleanup

--- nd2shrink/save.py
+++ nd2shrink/save.py
@@ -7,29 +7,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def series_tiff(fname:str, reader, rescale=4, to_8bits=False):
-    for i, well in enumerate(reader):
-        ds_well = transform.scale_down(well, rescale)
-        if to_8bits:
-            ds_well = transform.rescale_16_8bits(ds_well)
-        name = fname.replace('.tif',f'_Pos_{i:03d}.tif')
-        # ds_well.shape = 
-        imsave(name, ds_well, imagej=True) #TZCYXS order
-    return True
 
 def tiff(fname:str, array:numpy.ndarray):
     imsave(fname, array, imagej=True)
-    return True
-
-def append_tiff(path, reader, rescale=4):
-
-    # print(f'{well.shape} -> {ds_well.shape}')
-    # out = np.array(ds_wells, dtype='uint16')
-    # print(out.dtype)
-    # print(out.shape)
-    with TiffWriter(path, bigtiff=True, byteorder=None, append=False, imagej=False) as tif:
-        for well in reader:
-            ds_well = transform.scale_down(well, rescale)
-            tif.save(ds_well)
-    logger.info(f'Saved to {path}')
     return True
