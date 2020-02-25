78eab7de116fc519e931125b18e1b062d65d7665
Andrey  ARISTOV <andrey.aristov@pasteur.frsource>
2020-02-26 00:07:34 +0100

    add test with calibration

--- tests/test_write.py
+++ tests/test_write.py
@@ -8,8 +8,10 @@ def test_save_zeros():
     arr = numpy.random.randint(0, 2**12, (20,256,256), dtype='uint16')
 
     _ = save.tiff('zeros.tif', arr)
+    _ = save.tiff('zeros_cal.tif', arr, calibration_um=0.1)
 
     assert os.path.exists('zeros.tif')
+    assert os.path.exists('zeros_cal.tif')
 
     tif = read.tiff('zeros.tif')
     numpy.testing.assert_array_equal(arr, tif)
