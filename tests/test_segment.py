fb63bdbbc856a835f8999d9ac30f096da8a0ca1d
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-03-02 16:38:29 +0100

    update segmentation

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -9,13 +9,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+logger = logging.getLogger(__name__)
+
 def test_segment():
     image = np.zeros((600, 600))
-    cx, cy, b, a = 300, 350, 100, 220
+    cx, cy, b, a = 300, 350, 50, 100
     rr, cc = ellipse(cx, cy, b, a)
     image[rr, cc] = 1.
 
     mask = segment.findSpheroid(image, plot=0)
     props = segment.get_props(mask)
-    np.testing.assert_almost_equal(props['eccentricity'], (1 - b ** 2 / a ** 2) ** 0.5, 2)
+    logger.debug(f'len(props) {len(props)}')
+    np.testing.assert_almost_equal(props[0]['eccentricity'], (1 - b ** 2 / a ** 2) ** 0.5, 0.02)
 
