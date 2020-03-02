932ae4a02eae8f1dea57c591bd1537bed38b5bed
Andrey  ARISTOV <andrey.aristov@pasteur.frsource>
2020-03-02 23:51:10 +0100

    add erosion, ecc limits

--- nd2_combine/segment.py
+++ nd2_combine/segment.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from skimage.measure import regionprops, label
-from scipy.ndimage import gaussian_filter
+from scipy.ndimage import gaussian_filter, binary_erosion
 import matplotlib.pyplot as plt
 import matplotlib
 import logging
@@ -10,13 +10,15 @@ matplotlib.logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def findSpheroid(
     imCropped:np.ndarray,
     sigma:float = 5,
+    erode:int = 3,
     threshold:float=0.5,
     max_ecc:float = 0.95,
-    max_major_axis_length=300,
+    lim_major_axis_length:tuple=(100, 300),
     plot:bool=False
 ):
 
@@ -45,7 +47,10 @@ def findSpheroid(
     imThresh = toThresh > np.max(toThresh) * threshold
 
     cnts, h = cv2.findContours(imThresh.astype('uint8'), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    temp = cv2.drawContours(imThresh.astype('uint8'), cnts, -1, (255,255,255), thickness=cv2.FILLED)
+    mask = cv2.drawContours(imThresh.astype('uint8'), cnts, -1, (255,255,255), thickness=cv2.FILLED)
+    eroded_mask = binary_erosion(mask, structure=np.ones((erode, erode))).astype(mask.dtype)
+
+    temp = eroded_mask
 
     imLabel = label(temp)
     logger.debug(f'found {imLabel.max()} regions')
@@ -58,13 +63,13 @@ def findSpheroid(
 
         if region.eccentricity > max_ecc:
         #check it is inside or outside
-            logger.debug(f'eccentricity {region.eccentricity} > 0.8')
+            logger.debug(f'eccentricity {region.eccentricity} > {max_ecc}')
 
             temp[imLabel == region.label] = 0
             #region given same value as sph. border
 
-        if region.major_axis_length > max_major_axis_length:
-            logger.debug(f'major_axis_length {region.major_axis_length} > max_major_axis_length {max_major_axis_length}')
+        if region.major_axis_length < lim_major_axis_length[0] or region.major_axis_length > lim_major_axis_length[1]:
+            logger.debug(f'major_axis_length {region.major_axis_length} outside limits {lim_major_axis_length}')
             temp[imLabel == region.label] = 0
 
     if plot:
@@ -74,8 +79,8 @@ def findSpheroid(
     return temp
 
 
-def get_props(mask:np.ndarray):
+def get_props(mask:np.ndarray, **kwargs):
     lab = label(mask)
     # assert lab.max() == 1, f'number of segments is {lab.max()}'
     reg = regionprops(lab)
-    return [{'area': r.area, 'eccentricity': r.eccentricity, 'major_axis_length': r.major_axis_length} for r in reg]
+    return [{'area': r.area, 'eccentricity': r.eccentricity, 'major_axis_length': r.major_axis_length, **kwargs} for r in reg]
