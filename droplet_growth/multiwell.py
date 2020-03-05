8b731856bdafc56242774c419f08f4e99e4f2b68
Andrey  ARISTOV <andrey.aristov@pasteur.frsource>
2020-03-05 04:16:23 +0100

    add bf segmentation

--- droplet_growth/multiwell.py
+++ droplet_growth/multiwell.py
@@ -2,6 +2,7 @@ import numpy as np
 import pims_nd2 as nd
 import json
 import logging
+import matplotlib.pyplot as plt
 
 
 logger = logging.getLogger(__name__)
@@ -27,5 +28,23 @@ def read_stitched_nd2(path: str, bundle="zyx", channel=0, time_limit=None):
             yield zyx
 
 
-def detect_wells(zyx: np.ndarray):
-    pass
+def detect_wells(bf: np.ndarray, thr=0.1, sigma=2, erode=5, plot=False):
+    grad = get_2d_gradient(bf)
+    sm_grad = gaussian_filter(grad, sigma)
+    mask = sm_grad > sm_grad.max() * thr
+    filled_mask = binary_erosion(binary_fill_holes(mask), structure=np.ones((erode,erode)))
+
+    if plot:
+        [show(b) for b in [grad, sm_grad, mask, filled_mask]]
+
+    return filled_mask
+
+
+def get_2d_gradient(xy):
+    gx, gy = np.gradient(xy)
+    return np.sqrt(gx ** 2 + gy ** 2)
+
+
+def show(grad, **kwargs):
+    plt.figure(figsize=(15, 10))
+    plt.imshow(grad, cmap='gray', **kwargs)
