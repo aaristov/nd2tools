a2e4da43930dc070f6b433c38264f21ad84defe9
Andrey  ARISTOV <aaristov@pasteur.fr>
2020-04-07 00:36:00 +0200

    update CLI help, update defauld suffix, search pattern by glob if needed

--- segment/__main__.py
+++ segment/__main__.py
@@ -8,6 +8,7 @@ from functools import partial
 import logging
 import click
 from multiprocessing import Pool, cpu_count
+from glob import glob
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -17,21 +18,36 @@ logger = logging.getLogger(__name__)
 @click.option(
     '--out_dir_suffix', '-o',
     # type=click.Path(),
-    default='',
-    help='Where to save the preview png for segmentation. Default: filename without .nd2'
+    default='_segment',
+    show_default=True,
+    help='Where to save the preview png for segmentation.'
 )
-@click.option('--len_range_px', '-l', type=(int, int), default=(50,500), show_default=True)
-@click.option('--log', type=str, default='info', show_default=True)
+@click.option('--len_range_px', '-l', type=(int, int), default=(50,500), show_default=True, help='min, max length of major axis in pixels')
+@click.option('--log', type=str, default='info', show_default=True, help='Logging level')
 
 def main(path:list=[], out_dir_suffix:str='', len_range_px:tuple=(50,500), log='info'):
 
     logging.basicConfig(level=getattr(logging, log.upper()))
     logger = logging.getLogger(__name__)
     logger.setLevel(getattr(logging, log.upper()))
-    logger.info(f'Total {len(path)} files')
+    
+    flist = check_paths(path)
+    logger.info(f'Total {len(flist)} files')
+
     fun = partial(process, out_dir_suffix=out_dir_suffix, len_range_px=len_range_px, logger=logger)
-    _ = list(map(fun, path))
+    _ = list(map(fun, flist))
     return True
+
+def check_paths(paths:list):
+    flist = []
+    for p in paths:
+        if os.path.exists(p):
+            flist.append(p)
+        else:
+            pp = glob(p)
+            [flist.append(ppp) for ppp in check_paths(pp)]
+    return flist
+
 
 def process(path:str='', out_dir_suffix:str='', len_range_px:tuple=(50,500), logger=logging):
     logger.info(f'Processing {path}')
@@ -40,9 +56,10 @@ def process(path:str='', out_dir_suffix:str='', len_range_px:tuple=(50,500), log
     dirr = create_results_dir(path, suffix=out_dir_suffix)
 
     try:
-        p = Pool(processes=cpu_count())
+        p = Pool(processes=(c := cpu_count()))
+        logger.info(f'Processing using pool of {c} workers')
         res = p.map(
-            partial(seg.crop_and_segment, dirr=dirr, lim_major_axis_length=len_range_px),
+            partial(seg.crop_and_segment, dirr=dirr, lim_major_axis_length=len_range_px, print_dot=True),
             reader
         )
     except Exception as e:
